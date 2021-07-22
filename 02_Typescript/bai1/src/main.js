7bd41e84513dae37973714e1d53db7cf6a699e9c
thanhtai18021994 <taibui18021994@gmail.com>
2021-07-22 10:16:47 +0700

    day2

---
+++ 02_Typescript/bai1/src/main.js
@@ -0,0 +1,34 @@
+var width;
+var height;
+width = 10.5;
+height = 20.5;
+var area = width * height;
+console.log("Dien tich hinh chu nhat " + area);
+var sum = 0;
+var count = 0;
+for (var i = 2; count < 30; i++) {
+    var isPrime = true;
+    if (i == 2) {
+        sum = sum + i;
+        continue;
+    }
+    for (var j = 0; j < i; j++) {
+        if (i % j == 0) {
+            isPrime = false;
+        }
+    }
+    if (!isPrime) {
+        continue;
+    }
+    sum = sum + i;
+    count++;
+}
+console.log("Tong 30 so nguyen to la " + sum);
+let obj=new Promise(resovle,reject=>{
+    let xml=new XMLHttpRequest;
+    xml.onload(function (){
+        if (this.status==200){
+            resovle()
+        }
+    })
+})
